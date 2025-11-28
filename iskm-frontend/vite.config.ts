f1fddc751bece541111056502d4d45d46cebd8e6
kumarcharan00 <kumarcharan463@gmail.com>
2025-11-28 17:13:41 +0530

    Adding allowed host list

--- iskm-frontend/vite.config.ts
+++ iskm-frontend/vite.config.ts
@@ -4,10 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), "");
+    const allowedHostNames = env.ALLOWED_HOSTS?.split(",");
     return {
         plugins: [react()],
         preview: {
             host: "0.0.0.0",
+            allowedHosts: allowedHostNames,
             port: Number(env.PREVIEW_PORT) || 4183,
         },
         server: {
